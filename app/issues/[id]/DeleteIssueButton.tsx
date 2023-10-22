de45fcfc8fe1b859fcb4b13441003dfa215db194
Davey Schulz <davey.schulz@hotmail.com>
2023-10-22 10:05:47 +0200

    Delete issue functionality

---
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { FC, useState } from 'react';
+
+interface DeleteIssueButtonProps {
+  issueId: number;
+}
+
+const DeleteIssueButton: FC<DeleteIssueButtonProps> = ({ issueId }) => {
+  const router = useRouter();
+  const [error, setError] = useState<boolean>(false);
+
+  const deleteIssueHandler = async () => {
+    try {
+      await axios.delete('/api/issues/' + issueId);
+      router.push('/issues');
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue? This action cannot be
+            undone.
+          </AlertDialog.Description>
+          <Flex gap="3" mt="4">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button onClick={deleteIssueHandler} color="red">
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            This issue could not be deleted
+          </AlertDialog.Description>
+          <Button
+            onClick={() => setError(false)}
+            mt="2"
+            color="gray"
+            variant="soft"
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
+  );
+};
+
+export default DeleteIssueButton;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,10 @@
 import prisma from '@/prisma/client';
-import { Box, Grid } from '@radix-ui/themes';
+import { Box, Flex, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import { FC } from 'react';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
+import DeleteIssueButton from './DeleteIssueButton';
 
 interface IssueDetailPageProps {
   params: { id: string };
@@ -17,12 +18,15 @@ const IssueDetailPage: FC<IssueDetailPageProps> = async ({ params }) => {
   if (!issue) notFound();
 
   return (
-    <Grid columns={{ initial: '1', md: '2' }} gap="5">
-      <Box>
+    <Grid columns={{ initial: '1', md: '5' }} gap="5">
+      <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <EditIssueButton issueId={issue.id} />
+        <Flex direction="column" gap="4">
+          <EditIssueButton issueId={issue.id} />
+          <DeleteIssueButton issueId={issue.id} />
+        </Flex>
       </Box>
     </Grid>
   );
