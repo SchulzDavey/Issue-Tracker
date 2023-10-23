9c468fd481f2c05313a701d55aa6e06f535b400c
Davey Schulz <davey.schulz@hotmail.com>
2023-10-23 11:35:49 +0200

    build issue chart summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from '@/prisma/client';
 import IssueSummary from './IssueSummary';
 import LatestIssues from './LatestIssues';
+import IssueChart from './IssueChart';
 
 const Home = async () => {
   const open = await prisma.issue.count({ where: { status: 'OPEN' } });
@@ -9,7 +10,7 @@ const Home = async () => {
   });
   const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
 
-  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
+  return <IssueChart open={open} inProgress={inProgress} closed={closed} />;
 };
 
 export default Home;
